61d5ec9faca10931b9a223e5ba4000dff988fe74
robocop019 <chrisbowen@comcast.net>
2019-06-11 16:51:45 -0500

    added user model and database data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_204912) do
+ActiveRecord::Schema.define(version: 2019_06_11_214806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2019_06_11_204912) do
     t.string "arrival_airport"
     t.string "departure_airport"
     t.datetime "boarding_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.boolean "is_employee", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-Flight.create(status: "On Time", airline: "American Airlines", boarding_time: Time.now + 2.days, departure_airport: "SNA", arrival_airport: "MIA", departure_time: Time.now + 2.days + 1.hour, arrival_time: Time.now + 3.days)
-Flight.create(status: "Cancelled", airline: "United Airlines", boarding_time: Time.now + 10.days, departure_airport: "RSW", arrival_airport: "ORD", departure_time: Time.now + 10.days + 1.hour, arrival_time: Time.now + 10.days + 3.hours)
-Flight.create(status: "Departed", airline: "American Airlines", boarding_time: Time.now - 1.day, departure_airport: "ORD", arrival_airport: "DFW", departure_time: Time.now - 1.day + 1.hour, arrival_time: Time.now)
+# Flight.create(status: "On Time", airline: "American Airlines", boarding_time: Time.now + 2.days, departure_airport: "SNA", arrival_airport: "MIA", departure_time: Time.now + 2.days + 1.hour, arrival_time: Time.now + 3.days)
+# Flight.create(status: "Cancelled", airline: "United Airlines", boarding_time: Time.now + 10.days, departure_airport: "RSW", arrival_airport: "ORD", departure_time: Time.now + 10.days + 1.hour, arrival_time: Time.now + 10.days + 3.hours)
+# Flight.create(status: "Departed", airline: "American Airlines", boarding_time: Time.now - 1.day, departure_airport: "ORD", arrival_airport: "DFW", departure_time: Time.now - 1.day + 1.hour, arrival_time: Time.now)
+
+User.create(first_name: "Sinan", last_name: "Fun", email: "funs@funny.com", phone_number: "123-456-7891")
