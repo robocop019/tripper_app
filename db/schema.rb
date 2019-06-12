7ef74a2a5c4777204dd567d4fb625e1d9783e331
ThundaHorse <abeeeeekim@gmail.com>
2019-06-12 14:04:35 -0500

    associations for flights, users, tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_214806) do
+ActiveRecord::Schema.define(version: 2019_06_12_190050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2019_06_11_214806) do
     t.string "arrival_airport"
     t.string "departure_airport"
     t.datetime "boarding_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "flight_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
